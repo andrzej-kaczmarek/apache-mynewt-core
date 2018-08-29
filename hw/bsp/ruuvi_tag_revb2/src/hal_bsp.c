45e1cee241f962326b6c23506ce4991b091e8c6f
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2018-08-29 12:55:53 +0200

    [ruuvi]

--- hw/bsp/ruuvi_tag_revb2/src/hal_bsp.c
+++ hw/bsp/ruuvi_tag_revb2/src/hal_bsp.c
@@ -17,6 +17,8 @@
  * under the License.
  */
 
+#define MMGR 0
+
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
@@ -30,7 +32,11 @@
 #include "hal/hal_spi.h"
 #include "hal/hal_watchdog.h"
 #include "hal/hal_i2c.h"
+#include "hal/hal_gpio.h"
 #include "mcu/nrf52_hal.h"
+#if MMGR
+#include "motion_mgr/motion_mgr.h"
+#endif
 #if MYNEWT_VAL(UART_0) || MYNEWT_VAL(UART_1)
 #include "uart/uart.h"
 #endif
@@ -57,8 +63,8 @@
 static struct bme280 bme280;
 #endif
 
-#if MYNEWT_VAL(LIS2DH12_ONB)
 #include <lis2dh12/lis2dh12.h>
+#if MYNEWT_VAL(LIS2DH12_ONB)
 static struct lis2dh12 lis2dh12;
 #endif
 
@@ -147,6 +153,13 @@ int spwm_idx[MYNEWT_VAL(SOFT_PWM_DEVS)];
 #endif
 
 #if MYNEWT_VAL(I2C_0)
+struct os_mutex g_bsp_i2c0_itf_lock;
+
+static int
+bsp_i2c0_itf_lock_init(void)
+{
+    return os_mutex_init(&g_bsp_i2c0_itf_lock);
+}
 static const struct nrf52_hal_i2c_cfg hal_i2c_cfg = {
     .scl_pin = MYNEWT_VAL(I2C_0_PIN_SCL),
     .sda_pin = MYNEWT_VAL(I2C_0_PIN_SDA),
@@ -213,6 +226,10 @@ hal_bsp_get_nvic_priority(int irq_num, uint32_t pri)
     }
     return cfg_pri;
 }
+
+#if MMGR
+motion_mgr_sensor_t bsp_motion_mgr_sensor;
+#endif
 
 /**
  * BME280 Sensor default configuration
@@ -282,6 +299,182 @@ config_lis2dh12_sensor(void)
 #endif
     return 0;
 }
+
+#if MMGR
+struct lis2dh12 bsp_motion_sensor;
+/*
+const struct lis2dh12_notif_cfg lis2dh12_ncfg[] = {
+    {
+      .event     = SENSOR_EVENT_TYPE_SINGLE_TAP,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_CLICK_SRC_SCLICK,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_CLICK
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_DOUBLE_TAP,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_CLICK_SRC_DCLICK,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_CLICK
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_SLEEP,
+      .int_num   = 1,
+      .notif_src = 0,
+      .int_cfg   = LIS2DH12_CTRL_REG6_I2_ACT
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_FREE_FALL,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_INT1_IA,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_IA2
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_WAKEUP,
+      .int_num    = 1,
+      .notif_src  = 0,
+      .int_cfg    = LIS2DH12_CTRL_REG6_I2_ACT
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_SLEEP_CHANGE,
+      .int_num   = 1,
+      .notif_src = 0,
+      .int_cfg   = LIS2DH12_CTRL_REG6_I2_ACT
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_ORIENT_CHANGE,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_INT1_IA,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_IA1
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_ORIENT_X_CHANGE,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_INT1_XH | LIS2DH12_INT1_XL,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_IA1
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_ORIENT_Y_CHANGE,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_INT1_YH | LIS2DH12_INT1_YL,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_IA1
+    },
+    {
+      .event     = SENSOR_EVENT_TYPE_ORIENT_Z_CHANGE,
+      .int_num   = 0,
+      .notif_src = LIS2DH12_INT1_ZH | LIS2DH12_INT1_ZL,
+      .int_cfg   = LIS2DH12_CTRL_REG3_I1_IA1
+    }
+};
+*/
+const struct lis2dh12_cfg bsp_motion_init_cfg =
+{
+    .lc_rate                      = MYNEWT_VAL(LIS2DH12_CFG_RATE),
+    .lc_fs                        = MYNEWT_VAL(LIS2DH12_CFG_FS),
+
+    .reference                    = MYNEWT_VAL(LIS2DH12_CFG_REFERENCE),
+
+    /* Tap config */
+    .tap                          =
+    {
+        .en_xs                    = MYNEWT_VAL(LIS2DH12_CFG_TAP_EN_XS),
+        .en_ys                    = MYNEWT_VAL(LIS2DH12_CFG_TAP_EN_YS),
+        .en_zs                    = MYNEWT_VAL(LIS2DH12_CFG_TAP_EN_ZS),
+        .en_xd                    = MYNEWT_VAL(LIS2DH12_CFG_TAP_EN_XD),
+        .en_yd                    = MYNEWT_VAL(LIS2DH12_CFG_TAP_EN_YD),
+        .en_zd                    = MYNEWT_VAL(LIS2DH12_CFG_TAP_EN_ZD),
+        /* ths data is 5 bits, fs = +-2g */
+        .click_ths                = MYNEWT_VAL(LIS2DH12_CFG_TAP_THS),
+        /* shock is maximum time data can be over threshold to register as tap
+           0 = 2*1/ODR, LSB = 4*1/ODR */
+        .time_limit               = MYNEWT_VAL(LIS2DH12_CFG_TAP_TIME_LIMIT),
+        /* latency is time between taps in double tap, LSB = 1/ODR */
+        .time_latency             = MYNEWT_VAL(LIS2DH12_CFG_TAP_TIME_LATENCY),
+        /* quiet is time after tap data is just below threshold
+           1/ODR */
+        .time_window              = MYNEWT_VAL(LIS2DH12_CFG_TAP_TIME_WINDOW),
+    },
+
+    /* Read mode config */
+    .read_mode                    =
+    {
+        .mode                     = MYNEWT_VAL(LIS2DH12_CFG_READ_MODE_MODE),
+        .int_num                  = MYNEWT_VAL(LIS2DH12_CFG_READ_MODE_INT_NUM),
+        .int_cfg                  = MYNEWT_VAL(LIS2DH12_CFG_READ_MODE_INT_CFG),
+    },
+
+    /* interrupt config */
+    .int_cfg =
+    {
+        {
+            .cfg = (MYNEWT_VAL(LIS2DH12_CFG_INT1_XLIE) << 0) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT1_XHIE) << 1) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT1_YLIE) << 2) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT1_YHIE) << 3) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT1_ZLIE) << 4) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT1_ZHIE) << 5) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT1_AOI_6D) << 6),
+            .ths                  = MYNEWT_VAL(LIS2DH12_CFG_INT1_THS),
+            .dur                  = MYNEWT_VAL(LIS2DH12_CFG_INT1_DUR),
+        },
+        {
+            .cfg = (MYNEWT_VAL(LIS2DH12_CFG_INT2_XLIE) << 0) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT2_XHIE) << 1) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT2_YLIE) << 2) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT2_YHIE) << 3) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT2_ZLIE) << 4) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT2_ZHIE) << 5) |
+                (MYNEWT_VAL(LIS2DH12_CFG_INT2_AOI_6D) << 6),
+            .ths                  = MYNEWT_VAL(LIS2DH12_CFG_INT2_THS),
+            .dur                  = MYNEWT_VAL(LIS2DH12_CFG_INT2_DUR),
+        }
+    },
+
+    /* Power mode */
+    .power_mode                   = MYNEWT_VAL(LIS2DH12_CFG_POWER_MODE),
+
+    /* fifo  config */
+    .fifo_mode                    = MYNEWT_VAL(LIS2DH12_CFG_FIFO_MODE),
+    .fifo_watermark               = MYNEWT_VAL(LIS2DH12_CFG_FIFO_WATERMARK),
+
+    /* sleep/wakeup settings */
+    .act_ths                      = MYNEWT_VAL(LIS2DH12_CFG_ACT_THS),
+    .act_dur                      = MYNEWT_VAL(LIS2DH12_CFG_ACT_DUR),
+
+    .latch_int1                   = MYNEWT_VAL(LIS2DH12_CFG_LATCH_INT1),
+    .latch_int2                   = MYNEWT_VAL(LIS2DH12_CFG_LATCH_INT2),
+
+    //.notif_cfg                    = (struct lis2dh12_notif_cfg *)lis2dh12_ncfg,
+    //.max_num_notif                = sizeof(lis2dh12_ncfg)/sizeof(*lis2dh12_ncfg),
+
+    /* Sensor type mask to track enabled sensors */
+    .lc_s_mask                         = MYNEWT_VAL(LIS2DH12_CFG_MASK),
+};
+
+static struct sensor_itf bsp_msense_itf = {
+    .si_type = SENSOR_ITF_SPI,
+    .si_num  = 0,
+    .si_cs_pin = 8,
+    .si_ints = {
+    { ACC_INT1, 1, MYNEWT_VAL(LIS2DH12_INT1_CFG_TRIGGER) },
+    { ACC_INT2, 2, MYNEWT_VAL(LIS2DH12_INT2_CFG_TRIGGER) } },
+    // .si_lock = &g_bsp_i2c0_itf_lock
+};
+
+
+// XXX:
+//motion_mgr_sensor_t bsp_motion_mgr_sensor;
+#define SENSOR_MOTION_INIT_FN lis2dh12_init
+#define SENSOR_MOTION_INIT_DEVICE_SHELL lis2dh12_shell_init
+
+int bsp_lis2dh12_set_power_mode(uint8_t mode)
+{
+    int rc = 0;
+    // sensor_lock( &bsp_motion_sensor.sensor );
+    // TODO: Not yet implemented
+    // sensor_unlock( &bsp_motion_sensor.sensor );
+    return rc;
+}
+#endif
 
 static void
 sensor_dev_create(void)
@@ -403,6 +596,7 @@ hal_bsp_init(void)
 #endif
 
 #if MYNEWT_VAL(I2C_0)
+    bsp_i2c0_itf_lock_init();
     rc = hal_i2c_init(0, (void *)&hal_i2c_cfg);
     assert(rc == 0);
 #endif
@@ -430,4 +624,42 @@ hal_bsp_init(void)
 #endif
 
     sensor_dev_create();
+}
+
+#if MMGR
+static void bsp_init_sensors(void)
+{
+    int rc;
+    (void)rc;
+
+    #if MYNEWT_VAL(SENSOR_MOTION_ENABLE)
+    rc = motion_mgr_sensor_create(&bsp_motion_mgr_sensor,
+        (void*)&bsp_motion_sensor, "motion", SENSOR_MOTION_INIT_FN,
+        (void*)&bsp_msense_itf);
+    assert(rc == 0);
+    #endif
+
+    #if MYNEWT_VAL(SENSOR_MOTION_ENABLE)
+    motion_mgr_init(&bsp_motion_mgr_sensor);
+
+    rc = motion_mgr_config_sensor(&bsp_motion_mgr_sensor,
+        (void*)&bsp_motion_init_cfg);
+    assert(rc == 0);
+    #endif
+
+    #if MYNEWT_VAL(SENSOR_MOTION_ENABLE) && MYNEWT_VAL(SENSOR_MOTION_DRIVER_SHELL_ENABLE)
+    SENSOR_MOTION_INIT_DEVICE_SHELL();
+    #endif
+}
+#endif
+
+void bsp_pkg_init(void)
+{
+#if MYNEWT_VAL(I2C_0)
+    bsp_i2c0_itf_lock_init();
+#endif
+
+#if MMGR
+    bsp_init_sensors();
+#endif
 }
